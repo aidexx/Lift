e1b93e95ae68a4ee2e7eb3cbb769ace3d36576b9
aidexx <yermakovich.nikita@gmail.com>
2022-06-22 16:27:39 +0300

    Добавьте файлы проекта.

---
+++ Lift/Interfaces/ILIftable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lift
+{
+    public interface ILIftable
+    {
+        protected IEnumerable<Request> RequestQueue { get; set; }
+        public int CurrentFloor { get; set; }
+        public LiftState State { get; set; }
+
+        public void Call(Request request);
+        public void Stop();
+        public void Cancel();
+
+    }
+
+    public enum LiftState
+    {
+        Still, MovingUp, MovingDown, Stopped
+    }
+}
---
+++ Lift/Interfaces/Request.cs
@@ -0,0 +1,9 @@
+﻿namespace Lift
+{
+    public class Request
+    {
+        public int Floor { get; set; }
+
+        public Request(int floor) => Floor = floor;
+    }
+}
